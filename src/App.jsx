106ed3df516e2222b71563b3e951f88e16d1b05f
samarpan020 <samarpan020@gmail.com>
2025-02-09 15:53:22 +0545

    Basic form added for API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-  
-      <div>
-        <p>Rabin Regmi</p>
- </div>
-    
-  )
+    <div>
+      <h1>Book Explorer</h1>
+      <p>The homepage to the book explorer page</p>
+      <form action="">
+        <input type="text" name="bookname" placeholder="Enter the book name"/> <br /> <br />
+        <input type="submit" value="Submit" ></input>
+      </form>
+    </div>
+  );
 }
 
 export default App;
